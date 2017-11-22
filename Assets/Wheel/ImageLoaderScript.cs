09ac397ab120a74c6d761bbb8c5ae91031f5b958
pranavpaharia <pranavpaharia@gmail.com>
2017-11-23 01:55:30 +0530

    Added Whole Project Files

---
+++ Assets/Wheel/ImageLoaderScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ImageLoaderScript : MonoBehaviour {
+
+    // Use this for initialization
+    SpriteRenderer wheelImgRender;
+    public string url;
+	void Start () {
+        wheelImgRender = this.GetComponent<SpriteRenderer>() as SpriteRenderer;
+
+        
+	
+	}
+
+
+    IEnumerator LoadImage()
+    {
+        WWW www = new WWW(url);
+        yield return www;
+
+  //      wheelImgRender.sprite = Sprite.Create(www.texture, new Rect(new Vector2(0, 0), new Vector2(www.texture.width / 2, www.texture.height)), 
+  //                                              new Vector2(this.transform.localPosition.x / 2, this.transform.localPosition.y / 2)
+   //                                             );
+    }
+
+    public void TriggerLoadImage()
+    {
+        StartCoroutine("LoadImage");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Wheel/MoveWheel.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveWheel : MonoBehaviour {
+
+	// Use this for initialization
+	Transform wheelTransform;
+    Rigidbody wheelrigidBody;
+    ImageLoaderScript imagescript;
+    GameObject[] crateObjects = new GameObject[30];
+	void Start () {
+		wheelTransform = this.gameObject.GetComponent<Transform>() as Transform;
+        wheelrigidBody = this.gameObject.GetComponent<Rigidbody>() as Rigidbody;
+        imagescript = this.gameObject.GetComponent<ImageLoaderScript>() as ImageLoaderScript;
+        crateObjects = GameObject.FindGameObjectsWithTag("Player");
+        InvokeRepeating("AddTorqueImpluse", 1,3);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // this.gameObject.transform.RotateAround(Vector3.forward, 10 * Time.deltaTime);
+        
+	
+	}
+
+    void AddTorqueImpluse()
+    {
+        float val = Random.Range(-1, 1);
+        wheelrigidBody.AddForceAtPosition( Vector3.right * 1000,  Vector3.down  , ForceMode.Impulse);
+        Debug.Log("Invoke Called");
+        foreach(GameObject value in crateObjects)
+        {
+            imagescript = value.GetComponent<ImageLoaderScript>() as ImageLoaderScript;
+            imagescript.TriggerLoadImage();
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+     //   wheelrigidBody.AddTorque(Vector3.forward * Time.fixedDeltaTime);
+    }
+}
